1382aa83fbb1b6edbf57bb8bd32b8229d15c8b93
Aung Naing Oo <aung0086@e.ntu.edu.sg>
2015-10-19 06:32:59 +0800

    First push for sign up form html css

---
+++ js/app.js
@@ -0,0 +1,61 @@
+//Problem: Hints are shown even when form is valid
+//Solution: Hide and show them at appropriate times
+var $password = $("#password");
+var $confirmPassword = $("#confirm_password");
+
+//Hide hints
+$("form span").hide();
+
+function isPasswordValid() {
+  return $password.val().length > 8;
+}
+
+function arePasswordsMatching() {
+  return $password.val() === $confirmPassword.val();
+}
+
+function canSubmit() {
+  return isPasswordValid() && arePasswordsMatching();
+}
+
+/*
+ * This function gets called 
+ * whenever there is some sort of action
+ * on the password element
+ * */
+function passwordEvent(){
+    //Find out if password is valid  
+    if(isPasswordValid()) {
+      //Hide hint if valid
+      $password.next().hide();
+    } else {
+      //else show hint
+      $password.next().show();
+    }
+}
+
+function confirmPasswordEvent() {
+  //Find out if password and confirmation match
+  if(arePasswordsMatching()) {
+    //Hide hint if match
+    $confirmPassword.next().hide();
+  } else {
+    //else show hint 
+    $confirmPassword.next().show();
+  }
+}
+
+/*if the passwords don't match, disable the submit event button*/
+/* prop means property */
+function enableSubmitEvent() {
+  $("#submit").prop("disabled", !canSubmit());
+}
+
+//When event happens on password input, call these methods
+$password.focus(passwordEvent).keyup(passwordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
+
+//When event happens on confirmation input
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
+
+enableSubmitEvent();
+
